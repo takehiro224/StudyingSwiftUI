b5eea316277f5267ac63c7452d1b0d7153eebeef
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-26 16:40:55 +0900

    Initial Commit

---
+++ StudyingSwiftUI/StudyingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyingSwiftUIApp.swift
+//  StudyingSwiftUI
+//
+//  Created by 渡邊丈洋 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
